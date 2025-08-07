bd11cda973721db1bcc1832d511c0f536c7eb882
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-07 10:44:16 +0200

    commitment of equal side of array

---
+++ codeWars/findEvenIndex.js
@@ -0,0 +1,21 @@
+function findEvenIndex(arr) {
+   
+    let getIndex = arr.map((_,index) => index);
+    let countIndex = getIndex.length;
+    let index = Math.round(countIndex / 2);
+    let tryGet  = [];
+    let tryright  = [];
+    for(let key = 0; key < index; key++){
+       tryGet.push(arr[key]);
+    }
+     tryGet.pop()
+      for(let key = index; key < countIndex; key++){
+       tryright.push(arr[key]);
+    }
+
+    return tryGet.reduce((a,b) => a + b) === tryright.reduce((a,b) => a + b);
+
+    
+}
+let b = [2,3,5,6,5,3,2]
+console.log(findEvenIndex(b))
