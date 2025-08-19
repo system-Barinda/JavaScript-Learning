935009b68115ea40a2f4bec8c3f666f9a343164d
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-19 08:03:18 +0200

    learning the map in object

---
+++ javaScript ES6/Map1.js
@@ -0,0 +1,11 @@
+let persons = {
+    name:"barinda system sylvere",
+    age:23,
+    location:"Muhanga",
+    univeresity:"tumba college"
+}
+let barinda = new Map(Object.entries(persons));
+let ba = barinda.keys();
+
+
+console.log(barinda.get('name'))
