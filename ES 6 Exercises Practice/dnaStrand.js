c826de88dde780833004c8dbfeab90762fcdaaf3
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-19 11:00:53 +0200

    initial commitment of dnaStrand in exercises

---
+++ ES 6 Exercises Practice/dnaStrand.js
@@ -0,0 +1,13 @@
+function DNAStrand(dna) {
+  return dna.replace(/./g, function(c) {
+    return DNAStrand.pairs[c]
+  })
+}
+
+DNAStrand.pairs = {
+  A: 'T',
+  T: 'A',
+  C: 'G',
+  G: 'C',
+}
+console.log(DNAStrand('TTTT'))
