bb7a90bcc3b9b2e1504a73a2fb8cf0902b417d5f
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-22 13:02:54 +0200

    how to delete with proxy

---
+++ ES 6 Exercises Practice/profiler.js
@@ -0,0 +1,8 @@
+console.time("barinda system");
+
+let b = [1,2,3,4,5,6,67,12,13,43,54,667,78,9,9,45,34,23,23,22,33,55,66,77,88];
+for(let i = 0; i < b.length; i++){
+    console.log(b[i]);
+}
+
+console.timeEnd("barinda system");
