2536d47dc3ff4e968106495da88117d17640d6d4
system sylvere BARINDA <systembarinda@gmail.com>
2025-08-26 19:54:59 +0200

    initial commit to the file I shared

--- ES 6 Exercises Practice/group1.js
+++ ES 6 Exercises Practice/group1.js
@@ -1,3 +1,9 @@
 // let barinda = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 // let system = Object.groupBy(barinda,arr => (arr % 2  == 0 ?'even':'older' ));
 // console.log(system);
+
+let barinda = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let result = Object.groupBy(barinda,arr => {
+    return arr % 2 === 0?'even':'older'
+})
+console.log(result)
